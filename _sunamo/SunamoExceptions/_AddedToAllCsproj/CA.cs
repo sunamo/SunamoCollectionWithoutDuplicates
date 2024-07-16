c95aad2dca5bc620e876074903c268f7596bfd76
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 09:59:10 +0200

    before the second approach to remove *SunamoExceptions.cs

--- _sunamo/SunamoExceptions/_AddedToAllCsproj/CA.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/CA.cs
@@ -1,6 +1,6 @@
 namespace SunamoCollectionWithoutDuplicates._sunamo.SunamoExceptions._AddedToAllCsproj;
 
-internal class CASunamoExceptions
+internal class CA
 {
     internal static bool IsListStringWrappedInArray<T>(List<T> v2)
     {

f672fbd681b966daab30a5beb30f59cfe678dfe4
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 01:03:38 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoCollectionWithoutDuplicates/CollectionWithoutDuplicatesStringComparing.cs
+++ SunamoCollectionWithoutDuplicates/CollectionWithoutDuplicatesStringComparing.cs
@@ -1,6 +1,3 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-
 namespace SunamoCollectionWithoutDuplicates;
 
 public class CollectionWithoutDuplicatesStringComparing<T> : CollectionWithoutDuplicatesBase<T>
--- SunamoCollectionWithoutDuplicates/_sunamo/FS.cs
+++
@@ -1,9 +0,0 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-
-namespace SunamoCollectionWithoutDuplicates._sunamo;
-
-//namespace SunamoCollectionWithoutDuplicates._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FS
-{
-}
